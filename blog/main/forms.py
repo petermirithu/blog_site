b9f74efad01195f469f8cc725ac1c359e168fc0a
petermirithu <pyra_m.k@yahoo.com>
2019-11-29 20:51:40 +0300

    add a comment form

--- blog/main/forms.py
+++ blog/main/forms.py
@@ -6,7 +6,7 @@ class blogForm(FlaskForm):
   '''
   class defining how the blog form shall look like
   '''
-  category=SelectField(u'Enter Category of blog', choices =[('Lifestyle Blog','Lifestyle Blogs'),('Entertainment Blogs','Entertainment Blogs'),('Education Blogs','Education Blogs'),('Political Blogs','Political Blogs'),('Relationship Blogs','Relationship Blogs')])
+  category=SelectField(u'Enter Category of blog', choices =[('Lifestyle Blogs','Lifestyle Blogs'),('Entertainment Blogs','Entertainment Blogs'),('Education Blogs','Education Blogs'),('Political Blogs','Political Blogs'),('Relationship Blogs','Relationship Blogs')])
   title=StringField("Enter Blog's Title", validators=[Required()])
   body=TextAreaField('Write the blog')
   submit=SubmitField('Post')
@@ -17,4 +17,11 @@ class U_profileForm(FlaskForm):
   '''
   bio=TextAreaField('Enter you Bio', validators=[Required()])
   submit=SubmitField('Submit')
+
+class commentForm(FlaskForm):
+  '''
+  class that defines hopw the comment form shall look like
+  '''
+  body=TextAreaField('Enter your comment', validators=[Required()])
+  submit=SubmitField('Post Comment')
   
