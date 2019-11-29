f32ffd750677255cf335c6ce3127a02b76db05f2
petermirithu <pyra_m.k@yahoo.com>
2019-11-29 18:56:16 +0300

    add get function to display blogs by category

--- blog/db_class.py
+++ blog/db_class.py
@@ -1,4 +1,3 @@
-#pylint: skip-file
 from . import db
 from flask_login import UserMixin
 from datetime import datetime
@@ -64,10 +63,39 @@ class Blog(db.Model):
   title=db.Column(db.String(255))
   body=db.Column(db.String())
   posted_on=db.Column(db.DateTime,default=datetime.utcnow)
-  posted_by=db.column(db.String(255))
+  posted_by=db.Column(db.String(255))
   user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
   comment=db.relationship('Comment',backref='comment',lazy='dynamic')
 
+  def save_blog(self):
+    '''
+    function that saves a new blog to the database
+    '''
+    db.session.add(self)
+    db.session.commit()
+
+  @classmethod
+  def get_blogs(cls,category):
+    '''
+    function that gets blogs by category
+    '''
+    blogs=Blog.query.filter_by(category=category)
+    return blogs
+
+  @classmethod
+  def personal_blogs(cls,user_id):
+    '''
+    function that gets all blogs posted by a signle person
+    '''
+    personalblogs=Blog.query.filter_by(user_id=user_id)
+    return personalblogs
+
+  def __repr__(self)  :
+    '''
+    function that helps in debugging.
+    '''
+    return f'Blog {self.title}'
+    
 
 class Comment(db.Model):
   '''
@@ -81,6 +109,11 @@ class Comment(db.Model):
   posted_on=db.Column(db.DateTime,default=datetime.utcnow)
   blog_id=db.Column(db.Integer,db.ForeignKey('blogs.id'))
 
+  def __repr__(self)  :
+    '''
+    function that helps in debugging.
+    '''
+    return f'Comment {self.body}'
 
 
 
